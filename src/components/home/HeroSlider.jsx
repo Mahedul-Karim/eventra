03bba5a4240d69671f95b0c7506f318adce15db3
Mahedul-Karim <karimrupo21@gmail.com>
2025-05-06 11:58:43 +0600

    slider bug fix and event data added

--- src/components/home/HeroSlider.jsx
+++ src/components/home/HeroSlider.jsx
@@ -10,6 +10,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 import { HERO_SLIDER } from "@/lib/data";
+import Container from "../common/Container";
 
 const HeroSlides = ({
   imageUrl,
@@ -23,22 +24,24 @@ const HeroSlides = ({
       className="bg-[url('/assets/hero-1.avif')] bg-cover bg-center bg-no-repeat py-20 relative flex flex-col justify-center px-4 xs:px-6 sm:px-16 z-[1] h-full gap-4"
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-      <div className="absolute inset-0 bg-black/50 -z-1"></div>
-      <h1 className="text-3xl sm:text-4xl md:text-5xl text-white font-bold leading-[1.3] whitespace-pre-wrap">
-        {title}
-      </h1>
-      <p className="text-gray-200 text-sm sm:text-base md:text-lg mb-2">
-        {description}
-      </p>
+      <Container>
+        <div className="absolute inset-0 bg-black/50 -z-1"></div>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl text-white font-bold leading-[1.3] whitespace-pre-wrap">
+          {title}
+        </h1>
+        <p className="text-gray-200 text-sm sm:text-base md:text-lg mb-2">
+          {description}
+        </p>
 
-      <Link
-        to={buttonLink}
-        className={`${buttonVariants({
-          variant: "default",
-        })} !rounded-full !bg-white !font-semibold h-10 !text-dark self-start px-8 hover:scale-[1.1]`}
-      >
-        {buttonText}
-      </Link>
+        <Link
+          to={buttonLink}
+          className={`${buttonVariants({
+            variant: "default",
+          })} !rounded-full !bg-white !font-semibold h-10 !text-dark self-start px-8 hover:scale-[1.1]`}
+        >
+          {buttonText}
+        </Link>
+      </Container>
     </div>
   );
 };
@@ -47,7 +50,7 @@ const HeroSlider = () => {
   const swiperRef = useRef(null);
 
   return (
-    <div className="h-screen relative">
+    <div className="h-[100dvh] relative">
       <Swiper
         pagination={{ clickable: true }}
         loop
--- src/lib/data.js
+++ src/lib/data.js
@@ -41,3 +41,4 @@ export const HERO_SLIDER = [
     buttonLink: "/events",
   },
 ];
+
