764e731f6bf544719d63a45e020480a5cc1bc0a1
Mahedul-Karim <karimrupo21@gmail.com>
2025-05-08 09:38:01 +0600

    profile updation functionality created

--- src/components/profile/Info.jsx
+++ src/components/profile/Info.jsx
@@ -3,14 +3,19 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { useStore } from "@/context/Context";
 
 const Info = () => {
+
+
+  const { user } = useStore();
+  
+
   return (
     <Card className="bg-white shadow-none border-none">
       <CardHeader>
@@ -22,15 +27,15 @@ const Info = () => {
       <CardContent className="space-y-4">
         <div className="space-y-2">
           <Label className="text-dark">Your Name:</Label>
-          <Input disabled value={"John Doe"} className="!text-base" />
+          <Input disabled value={user?.displayName} className="!text-base" />
         </div>
         <div className="space-y-2">
           <Label className="text-dark">Your Email:</Label>
-          <Input disabled value={"test@gmail.com"} className="!text-base" />
+          <Input disabled value={user?.email} className="!text-base" />
         </div>
         <div className="space-y-2">
           <Label className="text-dark">Your Profile Link:</Label>
-          <Input disabled value={"unsplash.com"} className="!text-base" />
+          <Input disabled value={user?.photoURL} className="!text-base" />
         </div>
       </CardContent>
     </Card>
--- src/components/profile/Update.jsx
+++ src/components/profile/Update.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -10,8 +10,43 @@ import {
 import { Label } from "../ui/label";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { useStore } from "@/context/Context";
+import { toast } from "sonner";
+import { updateProfile } from "firebase/auth";
+import { auth } from "@/config/firebase.config";
 
 const Update = () => {
+  const { user, setUser } = useStore();
+
+  const [name, setName] = useState(user?.displayName || "");
+  const [photoUrl, setPhotoUrl] = useState(user?.photoURL || "");
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const updateUserProfile = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+
+       await updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photoUrl,
+      });
+
+
+      await auth.currentUser.reload();
+     
+      setUser(auth.currentUser);
+
+      toast.success("User updated successfully!");
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Card className="bg-white shadow-none border-none">
       <CardHeader>
@@ -21,17 +56,27 @@ const Update = () => {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={updateUserProfile}>
           <div className="space-y-2">
             <Label className="text-dark">Your Name:</Label>
-            <Input placeholder="Your New Name" />
+            <Input
+              placeholder="Your New Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
 
           <div className="space-y-2">
             <Label className="text-dark">Your Profile Link:</Label>
-            <Input placeholder="Your new image link" />
+            <Input
+              placeholder="Your new image link"
+              value={photoUrl}
+              onChange={(e) => setPhotoUrl(e.target.value)}
+            />
           </div>
-          <Button className="font-semibold w-full">Update</Button>
+          <Button className="font-semibold w-full" disabled={isLoading}>
+            Update
+          </Button>
         </form>
       </CardContent>
     </Card>
